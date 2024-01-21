5ff8cc585fa3ad01a68b9245009911093f602dc9
maria_koren <maria_koren@yahoo.com>
2024-01-21 10:10:17 +0100

    config mongodb connection

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+
+const app = express();
+dotenv.config();
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO)
+        console.log("connected to mongodb")
+    } catch (err) {
+        throw err
+    }
+}
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongoDB disconnected")
+})
+
+app.listen(8800, () => {
+    connect();
+    console.log("connected to backend");
+
+})
