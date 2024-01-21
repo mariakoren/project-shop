9ee66a68de89d5ba181eb58398322ac6caf3a069
maria_koren <maria_koren@yahoo.com>
2024-01-21 13:12:07 +0100

    homepage

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// import NavBar from '../../components/navbar/navbar.jsx';
+// import Header from '../../components/header/header.jsx';
+// import Featured from '../../components/featured/featured.jsx';
+
+const Home = () =>{
+    return (
+        <div>
+            {/* <NavBar/> */}
+            {/* <Header/> */}
+            <div className="homeContainer">
+                {/* <Featured/> */}
+                homepage
+            </div>
+        </div>
+
+    )
+}
+
+export default Home;
