8f78580aec68d6337bb8e2df0b6a6babb49e5898
maria_koren <maria_koren@yahoo.com>
2024-01-21 12:26:58 +0100

    comments crud

--- api/routers/comments.js
+++ api/routers/comments.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 import { createComment, deleteComment, getComments, updateComment } from "../controllers/comments.js";
-// import { createItem, deleteItem, getItem, getItems, updateItem } from "../controllers/items.js";
 
 const router = express.Router();
 
@@ -13,5 +12,4 @@ router.put("/:id", verifyAdmin, updateComment)
 router.delete("/:id", verifyAdmin, deleteComment)
 //get all
 router.get("/", getComments)
-
 export default router;
